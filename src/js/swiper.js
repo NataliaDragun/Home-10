8d1808c76cdd654b80d3f7da7d413ae347642dd0
NataliaDragun <natkarusak1994@gmail.com>
2023-02-22 22:12:04 +0300

    main page and mobile page

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,8 +2,8 @@ import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
     
 const swiper = new Swiper('.swiper', {
  // Optional parameters
- direction: 'vertical',
- loop: true,
+ direction: 'horizontal',
+  loop: true,
 
  // If we need pagination
  pagination: {
